5f3c6d87e0ccea15c2e089e24594c9114cd70c9b
Paphatsinee <paphatsinee.r@ku.th>
2021-02-22 18:28:53 +0700

    Update Chef.c

--- Chef.c
+++ Chef.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
 int main()
 {
+    int a,i,j;
+    scanf("%d",&a);
+    int n[a-1];
+    for(i=0;i<a;i++)
+    {
+        for(j=1;j<=a-1;j++)
+        {
+            scanf("%d",&n[j]);
+        }
+
+
+    }
 }
